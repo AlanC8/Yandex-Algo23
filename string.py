df9f2bb55c45c47044e3822c591ffffe3a877134
Alan <askvizi@bk.ru>
2023-01-21 22:19:25 +0600

    Casting

---
+++ string.py
@@ -0,0 +1,14 @@
+b = "Hello, World!"
+print(b[2:5])
+a = "Hello"
+
+bx = "World"
+c = a + bx
+print(c)
+
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
